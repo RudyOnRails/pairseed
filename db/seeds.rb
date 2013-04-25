dcbe25221e514303232250806f2d00f339185f3a
Kevin Musiorski <rudyonrails@gmail.com>
2013-04-25 00:01:09 -0500

    seed changes...

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,8 @@ User.delete_all
 OrganizationMembership.delete_all
 PairProposal.delete_all
 
-kevin = User.create(:first_name => "Kevin", :last_name => "Musiorski", :email => "musiorski@gmail.com", :password => "pairseed", :password_confirmation => "pairseed", :username => "RudyOnRails")
+kevin = User.create(:first_name => "Kevin", :last_name => "Musiorski", :email => "rudyonrails@gmail.com", :password => "pairseed", :password_confirmation => "pairseed", :username => "RudyOnRails")
+helper = User.create(:first_name => "Helper", :last_name => "McGee", :email => "helper@pairseed.com", :password => "pairseed", :password_confirmation => "pairseed", :username => "i_help_you")
 neal = User.create(:first_name => "Neal", :last_name => "Sales-Griffin", :email => "neal@pairseed.com", :password => "pairseed", :password_confirmation => "pairseed", :username => "Godfather")
 vince = User.create(:first_name => "Vince", :last_name => "Cabansag", :email => "vince@pairseed.com", :password => "pairseed", :password_confirmation => "pairseed", :username => "Vince123")
 zach = User.create(:first_name => "Zach", :last_name => "Zuber", :email => "zach@pairseed.com", :password => "pairseed", :password_confirmation => "pairseed", :username => "zzuber")
@@ -20,15 +21,15 @@ a_patel = User.create(:first_name => "Ankur", :last_name => "Patel", :email => "
 t_brown = User.create(:first_name => "Tom", :last_name => "Brown", :email => "tom_brown@pairseed.com", :password => "pairseed", :password_confirmation => "pairseed", :username => "t_bone")
 e_cooper = User.create(:first_name => "Emily", :last_name => "Cooper", :email => "emilymcooper@pairseed.com", :password => "pairseed", :password_confirmation => "pairseed", :username => "coop")
 
-kevin.pair_topics.create(:title => "Looking to get started with FactoryGirl.", :description => "I don't know much about it, but would like to go through it with someone.")
-kevin.pair_topics.create(:title => "if anyone has any experience with the google calendar API and would like to pair up and play around, give me a shout.", :description => "I don't know much about it, but would like to go through it with someone.")
-fonso.pair_topics.create(:title => "Error R11 (Bad bind) -> Process bound to port 3000, should be 12776", :description => "google is not helping me with this error. any one ever encounter this?")
-fonso.pair_topics.create(:title => "I'm trying to figure out what this font is.", :description => "I'm trying to figure out what this font is.")
-fonso.pair_topics.create(:title => "Has anyone had experience with turn.js ?", :description => "Want to play around and learn together.")
-h_twardosz.pair_topics.create(:title => "I'm working on a site using a squarespace template and now want to drop in the slider code.", :description => "I'm good until step 3..... thoughts?")
-a_patel.pair_topics.create(:title => "Our team is looking into developing with PhoneGap - does anyone have any expertise with PhoneGap development that is willing to jump on a 30 minute phone call with us?", :description => "We would truly appreciate the insight!")
-t_brown.pair_topics.create(:title => "Looking to go over the basics of git.", :description => "Just trying to get some basic concepts down and don't want to mess anything up!")
-e_cooper.pair_topics.create(:title => "I've been struggling for a week now trying to install the Nokogiri gem.", :description => " I haven't had success installing it with homebrew or macports.  Has anyone had a similar issue who can make a recommendation and/or screen share with me to figure this out?  Maybe I should try to manually install? ")
+kevin.pair_topics.create(:title => "Looking to get started with FactoryGirl.")
+kevin.pair_topics.create(:title => "if anyone has any experience with the google calendar API and would like to pair up and play around, give me a shout.")
+fonso.pair_topics.create(:title => "Error R11 (Bad bind) -> Process bound to port 3000, should be 12776")
+fonso.pair_topics.create(:title => "I'm trying to figure out what this font is.")
+fonso.pair_topics.create(:title => "Has anyone had experience with turn.js ?")
+h_twardosz.pair_topics.create(:title => "I'm working on a site using a squarespace template and now want to drop in the slider code.")
+a_patel.pair_topics.create(:title => "Our team is looking into developing with PhoneGap - does anyone have any expertise with PhoneGap development that is willing to jump on a 30 minute phone call with us?")
+t_brown.pair_topics.create(:title => "Looking to go over the basics of git.")
+e_cooper.pair_topics.create(:title => "I've been struggling for a week now trying to install the Nokogiri gem.")
 
 kevin.pair_places.create(:nickname => "C&C Starbucks", :street => "750 North Franklin Street", :city => "Chicago", :country => "USA")
 kevin.pair_places.create(:nickname => "Tinley Starbucks", :street => "15901 South Harlem Avenue", :city => "Chicago", :country => "USA")
