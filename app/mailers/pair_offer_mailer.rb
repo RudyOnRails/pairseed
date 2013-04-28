c58806fb2db5f17b5083356f48d5b1793fd6a883
Kevin Musiorski <rudyonrails@gmail.com>
2013-04-28 17:42:12 -0500

    Cleaned up pair_offer email

--- app/mailers/pair_offer_mailer.rb
+++ app/mailers/pair_offer_mailer.rb
@@ -4,6 +4,7 @@ class PairOfferMailer < ActionMailer::Base
   def basic_offer(pair_offer, pair_topic)
     @pair_offer = pair_offer
     @pair_topic = pair_topic
-    mail(from: "#{@pair_offer.user.first_name + " " + @pair_offer.user.last_name }", to: @pair_topic.user.email, subject: "#{@pair_offer.user.username} wants to pair!")
+    @helper = @pair_offer.user
+    mail(:from => "Pair Seed", to: @pair_topic.user.email, subject: "#{@helper.full_name + " (" + @helper.username + ") "} wants to pair!")
   end
 end
