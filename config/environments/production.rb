6fb3d60965a2e0147aef9673f3d322f22a80783b
Kevin Musiorski <rudyonrails@gmail.com>
2013-04-29 11:28:09 -0500

    need to config smtp for production environemnt, sendgrid service...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,15 @@ PairSeed::Application.configure do
   config.assets.initialize_on_precompile = false
 
   config.action_mailer.default_url_options = { host: 'pairseed.com' }
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
 end
