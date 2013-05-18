c2c721992bf51243548f0b8cfab66375f399f7b3
Kevin Musiorski <rudyonrails@gmail.com>
2013-05-18 17:02:19 -0500

    References #111. add offer_id attribute to appointment creation in accept action of offer controller

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -22,7 +22,7 @@ class OffersController < ApplicationController
       other_offer.decline
     end
     @topic.schedule
-    Appointment.create(:place => @offer.suggested_place, :date_and_time => @offer.suggested_datetime, :helper_id => @offer.user.id, :helpee_id => @topic.user.id, :topic_id => @topic.id)
+    Appointment.create(:offer_id => @offer.id, :place => @offer.suggested_place, :date_and_time => @offer.suggested_datetime, :helper_id => @offer.user.id, :helpee_id => @topic.user.id, :topic_id => @topic.id)
 
 
     redirect_to @topic
