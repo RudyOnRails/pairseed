96855779d4926997c7ef041552d31ed7a3b61342
Kevin Musiorski <rudyonrails@gmail.com>
2013-05-18 17:00:01 -0500

    References #111. appts#index sets two instance vars, scheduled and cancelled appts#cancel calls cancel_appointment event on both the appt's topic and the appt's offer

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,11 +1,14 @@
 class AppointmentsController < ApplicationController
   def index
-    @my_appointments = Appointment.where("helper_id = ? OR helpee_id = ?", current_user.id, current_user.id)
+    @my_appointments = Appointment.scheduled.where("helper_id = ? OR helpee_id = ?", current_user.id, current_user.id)
+    @my_cancelled_appointments = Appointment.cancelled.where("helper_id = ? OR helpee_id = ?", current_user.id, current_user.id)
   end
 
   def cancel
     @appointment = Appointment.find(params[:id])
     @appointment.cancel
+    @appointment.topic.cancel_appointment
+    @appointment.offer.cancel_appointment
     redirect_to :back
   end
 end
