baaeb98fef30f54b665635f085dc24f97cf2616c
Kevin Musiorski <rudyonrails@gmail.com>
2013-05-18 19:01:14 -0500

    References #111. offer has one appt...

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,7 @@ class Offer < ActiveRecord::Base
   attr_accessible :message, :suggested_datetime, :topic_id, :suggested_place, :user_id
   belongs_to :topic
   belongs_to :user
+  has_one :appointment
 
   state_machine :initial => :acceptable do
     event :accept do
