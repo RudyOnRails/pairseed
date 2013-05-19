a3ef6a89068c19236f5dabc3302b70d4e7803f5e
Kevin Musiorski <rudyonrails@gmail.com>
2013-05-18 19:12:02 -0500

    References #111. adding offer_id column to appointments table...

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20130517045339) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
     t.string   "state"
+    t.integer  "offer_id"
   end
 
   create_table "memberships", :force => true do |t|
